f32b166389537c3d281bba40bf250e28834870dc
Ryan Hill <rchrys.hill@gmail.com>
2024-05-15 20:38:52 +0100

    REF: Add configure options for group group and representation

---
+++ Global.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <config.h>
+
+#ifndef BKEEPER_GAUGEGROUP
+#define BKEEPER_DEFAULTGAUGEGROUP
+#define BKEEPER_GAUGEGROUP Grid::GroupName::SU
+#endif
+
+#ifndef BKEEPER_NC
+#define BKEEPER_DEFAULTNC
+#define BKEEPER_NC 3
+#endif
+
+#ifndef BKEEPER_REPRESENTATION
+#define BKEEPER_DEFAULTREPRESENTATION
+#define BKEEPER_REPRESENTATION RepresentationName::Fundamental
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,55 @@
 #include "Benchmark.hpp"
+#include "Global.h" // Contains macros for executeBenchmark template parameters
 
 
 int main(int argc, char** argv)
 {
-    initGrid();
-    executeBenchmark<::GroupName::SU, 3, RepresentationName::Fundamental>();
+    initGrid(argc, argv);
+
+    std::cout << GridLogMessage << "==================================================" << std::endl;
+    
+    #ifdef BKEEPER_DEFAULTGAUGEGROUP
+    std::cout << GridLogMessage << "No gauge group defined by autotools: defaulting to SU" << std::endl;
+    #endif
+
+    #ifdef BKEEPER_DEFAULTNC
+    std::cout << GridLogMessage << "No number of colours defined by autotools: defaulting to 3" << std::endl;
+    #endif
+
+    #ifdef BKEEPER_DEFAULTREPRESENTATION
+    std::cout << GridLogMessage << "No group representation defined by autotools: defaulting to Fundamental" << std::endl;
+    #endif
+
+    // Message to remind which group + rep the benchmark was compiled for.
+    std::cout << GridLogMessage << "Using ";
+    if constexpr(std::is_same_v<BKEEPER_GAUGEGROUP, Grid::GroupName::SU>)
+        std::cout << "SU";
+    else if constexpr(std::is_same_v<BKEEPER_GAUGEGROUP, Grid::GroupName::Sp>)
+        std::cout << "Sp";
+    std::cout << "(" << BKEEPER_NC << ")";
+    constexpr RepresentationName repname = BKEEPER_REPRESENTATION;
+    switch(repname)
+    {
+        case RepresentationName::Fundamental:
+            std::cout << ", Fundamental";
+            break;
+        case RepresentationName::Adjoint:
+            std::cout << ", Adjoint";
+            break;
+        case RepresentationName::TwoIndexSymmetric:
+            std::cout << ", TwoIndexSymmetric";
+            break;
+        case RepresentationName::TwoIndexAntiSymmetric:
+            std::cout << ", TwoIndexAntiSymmetric";
+            break;
+        default:
+            break;
+    }
+    std::cout << " representation" << std::endl;
+
+    std::cout << GridLogMessage << "==================================================" << std::endl;
+    
+    executeBenchmark<BKEEPER_GAUGEGROUP, BKEEPER_NC, BKEEPER_REPRESENTATION>();
     teardownGrid();
     return 0;
 }
