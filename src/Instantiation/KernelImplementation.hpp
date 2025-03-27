f8618e01bd7ef17878fd9e91bab03f5822ad0461
Ryan Hill <rchrys.hill@gmail.com>
2025-03-27 20:52:23 +0000

    Fix(?) AVX512 compilation

--- src/Instantiation/KernelImplementation.hpp
+++ src/Instantiation/KernelImplementation.hpp
@@ -5,13 +5,5 @@
 #include <Grid/qcd/action/fermion/FermionCore.h>
 #include <Grid/qcd/action/fermion/implementation/WilsonKernelsImplementation.h>
 #include <Grid/qcd/action/fermion/implementation/WilsonKernelsHandImplementation.h>
-
-#ifndef AVX512
-#ifndef QPX
-#ifndef A64FX
-#ifndef A64FXFIXEDSIZE
 #include <Grid/qcd/action/fermion/implementation/WilsonKernelsAsmImplementation.h>
-#endif
-#endif
-#endif
-#endif
+
